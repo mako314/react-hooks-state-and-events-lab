22a27d21b139469220de34d29ee014da52d48bac
Macolister <bispo.mac@gmail.com>
2023-05-24 21:10:40 -0400

    finish shopping list

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,18 +3,28 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
 
-  const [selectedCategory, filterBySelectedCategory] = useState("Filter by category");
+  const [selectedCategory, filterBySelectedCategory] = useState("All")
 
-  function filterMeFood(){
-
+  function handleFilterChange(event){
+    filterBySelectedCategory(event.target.value)
   }
+    const foodsToDisplay = items.filter((item) => {
+      if (selectedCategory === "All")
+      {
+        return item;
+      } else {
+        return item.category === selectedCategory;
+      }
+    })
+//move in category
+  
 
 
 
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter"  onChange={handleFilterChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -22,7 +32,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {foodsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
